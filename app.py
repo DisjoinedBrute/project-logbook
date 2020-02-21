b384d568e95749c07fb0c45950fa49aafa093087
Siddharth Iyer <siddharth1912@outlook.com>
2020-02-21 18:14:14 +0530

    project-logbook

---
+++ app.py
@@ -0,0 +1,59 @@
+import os
+import mysql.connector
+from flask import Flask, render_template, request, session
+
+app = Flask(__name__)
+
+# Database connection
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='sid@191203',
+    database='logbook'
+)
+
+mycursor = mydb.cursor()
+
+
+# Runs when the user goes to the URL
+@app.route('/')
+def index():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('dashboard.html')
+
+
+# When form sends back data
+@app.route('/auth', methods=['POST'])
+def login():
+    mycursor.execute(
+        f"SELECT * FROM users WHERE username = '{request.form['username']}' AND password = '{request.form['pass']}'")
+    myresult = mycursor.fetchall()
+    if len(myresult) > 0:
+        session['logged_in'] = True
+        return render_template('dashboard.html')
+    else:
+        return index()
+
+
+# When user tries to access dashboard
+@app.route('/dashboard')
+def viewdash():
+    if session.get('logged_in'):
+        return render_template('dashboard.html')
+    else:
+        return index()
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    mycursor.execute(
+        f"INSERT INTO `{request.form['class_section']}` (teacherid, date, subject, chaps) VALUES ('{request.form['teacherid']}', '{request.form['date']}' , '{request.form['subject']}', '{request.form['chapsdone']}')")
+    mydb.commit()
+    return render_template('login.html')
+
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.run(host='0.0.0.0', port=8080)  # Basically runs the website on localhost:8080
---
+++ mysql-generator.py
@@ -0,0 +1,27 @@
+  import mysql.connector
+
+  mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='sid@191203'
+  )
+
+  mycursor = mydb.cursor()
+  mycursor.execute('CREATE DATABASE logbook')
+  mycursor.execute("USE logbook")
+  mycursor.execute('CREATE TABLE users (teacherid VARCHAR(30), username  VARCHAR(30) , password VARCHAR(30))')
+  mycursor.execute('CREATE TABLE 11a (teacherid VARCHAR(25), date  VARCHAR(25) , subject VARCHAR(25), chaps TEXT) ')
+  mycursor.execute('CREATE TABLE 11b (teacherid VARCHAR(25), date  VARCHAR(25) , subject VARCHAR(25), chaps TEXT) ')
+  mycursor.execute('CREATE TABLE 11c (teacherid VARCHAR(25), date  VARCHAR(25) , subject VARCHAR(25), chaps TEXT) ')
+
+  sql = 'INSERT INTO users (teacherid, username , password) VALUES (%s, %s , %s)'
+  val = [
+      ('B/0001', 'Shobha' , 'shobha1234'),
+      ('B/0002', 'Vasudha' , 'vasudha1234'),
+      ('B/0003', 'Sai' , 'sai1234'),
+      ('B/0004', 'Rajeev' , 'rajeev1234'),
+      ('B/0005', 'Siddharth' , 'siddharth1234').
+  ]
+
+  mycursor.executemany(sql, val)
+  mydb.commit()
