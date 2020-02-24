0187eb94f7b698d00fdfe2f4d46dee9e2f91416f
Siddharth <siddharth1912@outlook.com>
2020-02-24 11:15:22 +0530

    Update mysql-generator.py

--- mysql-generator.py
+++ mysql-generator.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host='localhost',
   user='root',
-  passwd='sid@191203'
+  passwd='password'
 )
 
 mycursor = mydb.cursor()
@@ -15,13 +15,6 @@ mycursor.execute('CREATE TABLE 11b (teacherid VARCHAR(25), date  VARCHAR(25) , s
 mycursor.execute('CREATE TABLE 11c (teacherid VARCHAR(25), date  VARCHAR(25) , subject VARCHAR(25), chaps TEXT) ')
 
 sql = 'INSERT INTO users (teacherid, username , password) VALUES (%s, %s , %s)'
-val = [
-    ('B/0001', 'Shobha' , 'shobha1234'),
-    ('B/0002', 'Vasudha' , 'vasudha1234'),
-    ('B/0003', 'Sai' , 'sai1234'),
-    ('B/0004', 'Rajeev' , 'rajeev1234'),
-    ('B/0005', 'Siddharth' , 'siddharth1234')
-]
-
-mycursor.executemany(sql, val)
+val = ('teacherid', 'username' , 'password')
+mycursor.execute(sql, val)
 mydb.commit()
